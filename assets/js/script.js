00012118fede0f8ce71c40906bd53b08c4e0ff2c
James-Glennon <glennon.jjp91@gmail.com>
2022-09-30 23:25:17 +0000

    [script] code for division game changed to ensure answer is always a positive integer

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ function calculateCorrectAnswer() {
     } else if (operator === '-') {
         return [operand1 - operand2, 'subtract'];
     } else if (operator === '/') {
-        return [Math.floor(operand1 / operand2), 'division'];
+        return [operand1 / operand2, 'division'];
     }
     else {
         alert(`Unimplemented operator ${operator}`);
@@ -136,7 +136,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivitionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand2 > operand1 ? operand1 : operand2;
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '/'
 }
